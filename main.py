82766edde17da948f095ebf52cbc665fe5b621cd
jmoca <jmorenocaballero@safareyes.es>
2023-09-29 10:41:35 +0200

    Hecho el replace de . por STOP, me falta controlar si no introduce un . al final

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
 print("\t\t\tPROGRAMA TELEGRAMA\n")
 frase = input("Teclea el mensaje: ")
 print("Cadena tecleada: " + frase)
+print("Mensaje a enviar: ")
+fraseStop = frase.replace("."," STOP")+ "STOP"
+print(fraseStop)
