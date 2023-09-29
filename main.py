97ec5543fecc46a012f926f1deffd21f7c255b92
jmoca <jmorenocaballero@safareyes.es>
2023-09-29 10:54:20 +0200

    CONTROLADO SI NO PONE PUNTO FINAL.Me falta contar las palabras

--- main.py
+++ main.py
@@ -2,5 +2,8 @@ print("\t\t\tPROGRAMA TELEGRAMA\n")
 frase = input("Teclea el mensaje: ")
 print("Cadena tecleada: " + frase)
 print("Mensaje a enviar: ")
-fraseStop = frase.replace("."," STOP")+ "STOP"
+if frase[-1] != ".":
+    frase = frase + "."
+fraseStop = frase.replace(".", " STOP") + "STOP"
+
 print(fraseStop)
