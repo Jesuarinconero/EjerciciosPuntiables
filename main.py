e3c8e2a661d132fdef92b85274b0be8889d3360a
jmoca <jmorenocaballero@safareyes.es>
2023-10-03 09:27:42 +0200

    Terminado falta comentar

--- main.py
+++ main.py
@@ -7,19 +7,21 @@ if frase[-1] != ".":
 fraseStop = frase.replace(".", " STOP") + "STOP"
 print(fraseStop)
 
-frase = frase.replace(".", "")
-print(frase)
+frase = frase.replace(".", "").replace(",", "")
 frase = frase.split(" ")
 print(frase)
 larga = 0
 corto = 0
 longi_frase = (len(frase))
 for palabra in frase:
-    if palabra > 5:
-        larga = larga + frase
+    if len(palabra) > 5:
+        larga =larga + 1
     else:
-        corto = corto + frase
-print(frase)
-print(longi_frase)
-#print("La cadena contiene"+ long_frase + "palabras de las cuales" )#larga +  "tienen más de 5 letras.")
-#print("Por tanto, al precio de 0.25€/palabra tenemos "5" y a 0.50€/palabra hay otras 4.")
+        corto = corto + 1
+
+To_largo = larga*0.5
+To_corto = corto*0.25
+Total = To_corto+To_largo
+print("La cadena contiene " + str(longi_frase) + " palabras de las cuales " + str(larga) + " tienen más de 5 letras.")
+print("Por tanto, al precio de 0.25€/palabra tenemos "+str(corto)+" y a 0.50€/palabra hay otras "+str(larga)+".")
+print("Total: "+str(Total)+"€")
