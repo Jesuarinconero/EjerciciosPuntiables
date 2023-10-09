968bb06930aa6b47cba1abd7d964939bf53ece78
jmoca <114067390+jmoca@users.noreply.github.com>
2023-10-09 19:04:07 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,2 +1,27 @@
 #puntuable 3 ...en proceso
 #UIT-6.1
+
+print(f"\t\t\t\033[4;1m" + "PROGRAMA OPTIMIZACIÓN TELEGRAMA\n" + "\033[0;m")
+
+
+# print("Cadena recibida: " + args[0])
+def menu_op():
+    print("Menu de opciones")
+    print("================")
+
+    opciones = {
+           '1': ('Opción 1', normal),
+           '2': ('Opción 2', pre_redu),
+           '3': ('Opción 3', morse),
+           '4': ('Salir', salir)
+       }
+def normal():
+    print("precio normal")
+def pre_redu():
+    print("precio reducido")
+def morse():
+    print("codigo morse")
+def salir():
+    print('Saliendo')
+
+print(menu_op)
